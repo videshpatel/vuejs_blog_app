eb41378631b34300abc9fa9ec6c29bd58c2901d0
“Videsh <videsh.patel@gmail.com>
2019-04-09 21:10:42 -0500

    Created user auth and created the PostIndex vue.

---
+++ src/router.js
@@ -0,0 +1,52 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
+import Signup from "./views/Signup.vue";
+import Login from "./views/Login.vue";
+import Logout from "./views/Logout.vue";
+import PostsIndex from "./views/PostsIndex.vue";
+import axios from "axios";
+
+axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
+
+Vue.use(Router);
+
+export default new Router({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home
+    },
+    {
+      path: "/postsindex",
+      name: "postsindex",
+      component: PostsIndex
+    },
+    {
+      path: "/signup",
+      name: "signup",
+      component: Signup
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login
+    },
+    {
+      path: "/logout",
+      name: "logout",
+      component: Logout
+    },
+    {
+      path: "/about",
+      name: "about",
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ "./views/About.vue")
+    }
+  ]
+});
---
+++ src/views/PostsIndex.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <h1>All Posts</h1>
+    <div v-for="post in posts">
+      <h2>{{ post.user_id }}</h2>
+      <h2>{{ post.title }}</h2>
+      <h2>{{ post.body }}</h2>
+      <h2>{{ post.image }}</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data: function() {
+    return {
+      posts: []
+    };
+  },
+  created: function() {
+    axios.get("/api/posts").then(response => {
+      this.posts = response.data;
+    });
+  },
+  methods: {}
+};
+</script>
